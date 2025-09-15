2b0d6e95dac36f8301bc9bd80556354bcb73f792
chaoukat99 <chawkatomar@gmail.com>
2025-09-15 21:15:15 +0100

    Fisrt commit on master branch 15/09/2025

---
+++ FileSystem.js
@@ -0,0 +1,129 @@
+//  FIle handling
+
+
+// import 
+
+const { log } = require("console");
+const fs=require("fs");
+
+
+
+
+// READ FILES && READ DIRECTORIES 
+
+// fs.readFile("document.txt",(err,data)=>{
+//     if(!err){
+//         console.log("file data : "+data)
+//     }else{
+//         console.log("erreur");
+//     }
+// })
+
+
+
+// Read Binary File 
+
+// fs.readFile("index.html",(err,data)=>{
+//     if(!err){
+//         console.log(data.toString()) // lire le contenu en format texte
+//     }else{
+//         console.log("erreur");
+//     }
+// })
+
+
+
+// CREATE files  write on files  || DIR
+
+
+// fs.writeFile("document.txt","hello world",(err)=>{
+
+//     if(!err){
+//         console.log("fichier Créer ")
+//     }else{
+//         console.log(" Erreur")
+//     }
+// })
+
+
+// appendFile
+
+// fs.appendFile("document.txt","\n\nHello gomycoders",(err)=>{
+
+//     if(!err){
+//         console.log("fichier Créer ")
+//     }else{
+//         console.log(" Erreur")
+//     }
+// })
+
+
+// UPDATE files 
+// writefile
+
+
+// Rename File 
+
+// fs.rename("index.html","contact.html",(err)=>{
+//     if(!err){
+//         console.log("file renamed");
+        
+//     }else{
+//         console.log("erreur");
+        
+//     }
+// })
+
+
+
+// DELETE files  || DIR
+
+
+// fs.unlink("document.txt",(err)=>{
+// if(!err){
+//     console.log("file deleted");
+    
+// }else{
+//     console.log("Erreur");
+    
+// }
+// })
+
+
+
+
+
+
+// Read DIR
+
+// fs.readdir("assets",(err,files)=>{
+//     if(!err){
+//         console.log(files);
+//     }else{
+//         console.log("erreur");
+//     }
+// })
+
+
+// CREATE new DIR
+
+
+// fs.mkdir("images",(err)=>{
+//   if(!err){
+//     console.log("dossier crée")
+//   }else{
+//     console.log("erreur")
+//   }
+// })
+
+// DELETE FOLDER
+
+
+// fs.rmdir("assets",(err)=>{
+//   if(!err){
+//     console.log("dossier supprimé")
+//   }else{
+//     console.log("erreur")
+//     console.log(err);
+//   }
+// })
---
+++ Server.js
@@ -0,0 +1,35 @@
+// Importer le module Http
+
+const http=require("http"); // common js method 
+
+// import http from "http"   // module js method 
+
+
+
+
+
+const myServer=http.createServer((req,res)=>{
+
+
+
+    // Traitement du serveur 
+    if(req.url=="/users" && req.method=="POST"){
+
+        // traitement 
+
+        res.writeHead(200,"OK");
+        res.end("the server is working")
+    }else if(req.url=="/users" && req.method=="GET"){
+        res.writeHead(200,"OK");
+        res.end("Get method is working ");
+    }
+
+})
+
+
+// lancer le serveur 
+myServer.listen(7000,()=>{
+    console.log("server is running on http://localhost:7000");
+})
+
+
